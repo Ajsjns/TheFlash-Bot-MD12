3684a3eb6cb6809c1c437899ee167be200f2983f
Ajsjns <asbhhjagshgashja@gmail.com>
2024-10-11 17:40:31 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب رزان🌚💔: لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
+    let message = '* مــنــور يـا قــلــب رزان🌚💔: الـاوامــر اكــتــب (.اوامر) لتعرف جميع مهام البوت ⚡️*';
 await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
     conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
 };
